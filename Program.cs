e3b5e99af3a542e2dc2cf602e8e57f4d38d74ca4
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-02-08 00:21:39 +0530

    do

--- Program.cs
+++ Program.cs
@@ -13,13 +13,14 @@ namespace Inventory_Management_System
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                   
                     webBuilder.UseStartup<Startup>();
                 });
     }
